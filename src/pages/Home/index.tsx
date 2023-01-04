9ef704ec31b7744fb31d566e9fe6076153284b69
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-01-04 11:20:12 -0300

    iniciada a criação de moléculas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,11 +5,15 @@ import { TextAtom } from "../../components/atomos/TextAtom";
 import { DiApple } from "react-icons/di";
 import { InputAtom } from "../../components/atomos/InputAtom";
 import { AlertAtom } from "../../components/atomos/AlertAtom";
+import { NavBarMolecule } from "../../components/moleculas/NavBarMolecule";
 
 export default function Home() {
   const [value, setValue] = useState(false);
   return (
     <>
+
+      <NavBarMolecule />
+
       <TextAtom
         children
         className="text-sm text-red-400"
