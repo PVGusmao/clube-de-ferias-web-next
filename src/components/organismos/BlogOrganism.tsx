58f7fa874cd85d983eebb513f438d0f2a33591ac
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-01-06 18:08:37 -0300

    tentativa de implementação de slide show animado

--- src/components/organismos/BlogOrganism.tsx
+++ src/components/organismos/BlogOrganism.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import imageBlog1 from '../../assets/imageBlog1.png';
+import { minorCardBlog } from "../../constants";
+import { BackgroundImageAtom } from "../atomos/BackgroundImageAtom";
 
 import { TextAtom } from "../atomos/TextAtom";
 import { BlogCardMolecule } from "../moleculas/BlogCardMolecule";
@@ -19,9 +21,24 @@ export function BlogOrganism() {
         image={imageBlog1}
       />
 
-      {
-        
-      }
+      <div className="sm:hidden sm:w-auto w-96 flex flex-row sm:w-auto sm:flex-col justify-between  overflow-x-auto">
+        {
+          minorCardBlog.map((element, index) => (
+            <BackgroundImageAtom
+              className={`flex items-center text-center rounded-xl m-2 justify-between h-36 w-96`}
+              image={element.image}
+              key={index}
+            >
+              <TextAtom
+                children
+                className="w-full text-white font-bold text-sm"
+                text={element.title}
+              />
+            </BackgroundImageAtom>
+          ))
+        }
+      </div>
+
     </div>
   )
 }
