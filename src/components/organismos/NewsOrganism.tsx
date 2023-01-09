4d0a2dfb5d225268ef0618e28e61c026b25216f4
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-01-09 14:11:34 -0300

    Ajustes feitos

--- src/components/organismos/NewsOrganism.tsx
+++ src/components/organismos/NewsOrganism.tsx
@@ -9,26 +9,26 @@ import { IconAtom } from "../atomos/IconAtom";
 
 export function NewsOrganism() {
   return (
-    <div className="sm:flex h-[25.5rem] sm:h-[35.5rem] w-full justify-evenly bg-[#F3F3F3]">
+    <div className="sm:flex grid content-center h-[53.875rem] h-[25.5rem] sm:h-[35.5rem] w-full justify-evenly bg-[#F3F3F3]">
       <div className="grid content-center">
         <TextAtom
-          className="text-5xl	font-bold text-[#F20F0F] text-left"
+          className="sm:text-5xl text-3xl	font-bold text-[#F20F0F] sm:text-left"
           text="Quer saber mais sobre"
         />
         <TextAtom
-          className="text-5xl	font-bold text-[#F20F0F] text-left mb-5"
+          className="sm:text-5xl text-3xl	font-bold text-[#F20F0F] sm:text-left mb-5"
           text="o clube de ferias?"
         />
         <TextAtom
-          className="font-light	text-left text-2xl"
+          className="font-light	sm:text-left sm:text-2xl"
           text="Assine nossa news e receba novidades toda semana"
         />
         <TextAtom
-          className="font-light	text-left text-2xl"
+          className="font-light	sm:text-left sm:text-2xl"
           text="novidades toda semana"
         />
 
-        <div className="flex mt-10">
+        <div className="flex my-10">
           <div className="flex h-[3.4375rem] w-[10.75rem] mb-5 bg-[#252525] items-center justify-center rounded-lg mr-5">
             <IconAtom
               icon={AiFillApple}
@@ -49,19 +49,19 @@ export function NewsOrganism() {
           </div>
         </div>
       </div>
-      <div className="flex items-center flex-col justify-center">
+      <div className="flex items-center flex-col justify-center ">
         <Input
           placeholder="Nome"
-          className="bg-[#D9D9D98A] w-[29.375rem] rounded-full p-3 mb-4"
+          className="bg-[#D9D9D98A] w-[19.875rem] sm:w-[29.375rem] rounded-full p-3 mb-4"
           disableUnderline
         />
         <Input
           placeholder="Email"
-          className="bg-[#D9D9D98A] w-[29.375rem] rounded-full p-3"
+          className="bg-[#D9D9D98A] w-[19.875rem] sm:w-[29.375rem] rounded-full p-3"
           disableUnderline
         />
 
-        <div>
+        <div className="mt-10">
           <div className="flex items-center">
             <Checkbox />
             <TextAtom text="Eu concordo em receber comunicação" />
