dc49c4c4ea11b5aeeaa7265d97ffb4d5220e86ef
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-01-10 16:52:41 -0300

    implementado responsividade dos pacotes do simulador

--- src/context/MyContext.tsx
+++ src/context/MyContext.tsx
@@ -1,10 +1,13 @@
 import { createContext, useState } from 'react';
+import { CardSimulationProps } from '../components/moleculas/CardSimulationMolecule';
 
 export type IMyContext = {
   showMenu: boolean;
-  setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
   link: string;
+  packages: CardSimulationProps[];
+  setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
   setLink: React.Dispatch<React.SetStateAction<string>>;
+  setPackages: React.Dispatch<React.SetStateAction<CardSimulationProps[]>>;
 }
 
 export const MyContext = createContext<IMyContext | null>(null);
@@ -19,11 +22,13 @@ export function MyProvider({ children }: Props) {
 
   const [showMenu, setShowMenu] = useState<boolean>(false);
   const [link, setLink] = useState<string>('');
+  const [packages, setPackages] = useState<CardSimulationProps[]>([]);
 
   return (
     <MyContext.Provider value={{
       showMenu, setShowMenu,
       link, setLink,
+      packages, setPackages,
     } as IMyContext}>
       {children}
     </MyContext.Provider>
