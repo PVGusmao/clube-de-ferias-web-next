ed18dae36b17c69507d49bdcb37f6b080c0f40e3
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-01-10 17:45:44 -0300

    mudança no cardSimulation

--- src/components/moleculas/CardSimulationMolecule.tsx
+++ src/components/moleculas/CardSimulationMolecule.tsx
@@ -1,5 +1,5 @@
-
 import { Card, CardHeader, CardBody } from "@material-tailwind/react";
+import { Button } from "@mui/material";
 import { TextAtom } from "../atomos/TextAtom";
 
 export type CardSimulationProps = {
@@ -17,7 +17,6 @@ export default function CardSimulationMolecule({
   price,
   date,
 }: CardSimulationProps) {
-
   return (
     <Card className="w-[16.528125rem] h-[23.50625]">
       <CardHeader floated={false} className="">
@@ -31,37 +30,52 @@ export default function CardSimulationMolecule({
         />
       </CardHeader>
       <CardBody className="">
-        <div className="flex flex-row justify-between h-20 ">
+        <div className="flex flex-row justify-between">
           <TextAtom
             children
-            text={name}
+            text={name.split("-")[0]}
             className="font-bold text-black 
             overflow-hidden
             "
           />
         </div>
-        <div className="flex justify-start flex-col">
+        <div className="flex justify-start flex-col h-30">
           <TextAtom
             children
             text={subname}
-            className="text-xs text-left text-black"
+            className="text-sm text-left text-[#5B5B5B] font-bold h-12 "
           />
           <TextAtom
             children
             text={date}
             className="text-xs text-left text-black"
           />
-          <TextAtom children text="a partir de" className="text-ml text-left" />
-          <TextAtom
-            children
-            text={price}
-            className="text-xl text-[##F20F0F] text-left"
-          />
-          <TextAtom
-            children
-            text="para 2 pessoas"
-            className="text-ml text-left"
-          />
+          <div className="flex flex-row justify-between items-center">
+            <div>
+              <TextAtom
+                children
+                text="a partir de"
+                className="text-xs text-left mt-5"
+              />
+              <TextAtom
+                children
+                text={price}
+                className="text-xl text-[#F20F0F] text-left font-bold"
+              />
+              <TextAtom
+                children
+                text="para 2 pessoas"
+                className="text-xs text-left"
+              />
+            </div>
+            <div>
+              <TextAtom
+                children
+                text="Comprar"
+                className="mt-10 bg-[#F20F0F] h-10 w-20 flex justify-center items-center rounded-lg text-white text-sm font-bold"
+              />
+            </div>
+          </div>
         </div>
       </CardBody>
     </Card>
