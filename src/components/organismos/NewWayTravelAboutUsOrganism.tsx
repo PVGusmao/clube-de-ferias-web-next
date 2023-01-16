dc0964e52b285d1b2058e35b9084a6bed4052b0d
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-01-16 18:20:35 -0300

    ajustado responsividade

--- src/components/organismos/NewWayTravelAboutUsOrganism.tsx
+++ src/components/organismos/NewWayTravelAboutUsOrganism.tsx
@@ -5,17 +5,17 @@ export function NewWayTravelAboutUsOrganism() {
   return (
     <div className="flex flex-col items-center">
       <TextAtom
-        className="font-ubuntu text-2xl sm:text-5xl font-bold w-[100%] px-6 mt-10 mb-10"
+        className="font-ubuntu text-2xl sm:text-5xl font-bold sm:w-[70%] px-6 sm:px-0 mt-10 mb-10"
         text={NewWayTravel[0].text1}
       > </TextAtom>
 
       <TextAtom
-        className="font-ubuntu text-base sm:text-xl font-medium w-[100%] px-6 mb-10"
+        className="font-ubuntu text-base sm:text-xl font-medium sm:w-[60%] px-6 sm:px-0 mb-10"
         text={NewWayTravel[0].text2}
       > </TextAtom>
 
       <TextAtom
-        className="font-ubuntu text-base sm:text-xl font-medium w-[100%] px-6 mb-10"
+        className="font-ubuntu text-base sm:text-xl font-medium sm:w-[60%] sm:px-0 px-6 mb-10"
         text={NewWayTravel[0].text3}
       > </TextAtom>
     </div>
