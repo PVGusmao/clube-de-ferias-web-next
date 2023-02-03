7b64efbd2a56eda9da7a82da535295c1438eaeb2
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-02-03 09:51:18 -0300

    retirada rota do nrok

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://lpdell.local/api",
-  baseURL: "http://ab71-189-112-253-186.sa.ngrok.io/api",
-  //
+  baseURL: "http://lpdell.local/api",
 });
 
 export default api;
