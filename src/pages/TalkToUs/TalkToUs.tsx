d66758f90e09578c842ed029ccab8b0269e04b0b
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-02-16 11:23:20 -0300

    finalizado o fale conosco

--- src/pages/TalkToUs/TalkToUs.tsx
+++ src/pages/TalkToUs/TalkToUs.tsx
@@ -4,14 +4,14 @@ import { FormOrganism } from "../../components/organismos/FormOrganism";
 export function TalkToUs() {
   return (
     <>
-      {/* <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center ">
         <TextAtom
           children
           className=" text-[25px] sm:text-[44px] font-bold  mt-10"
           text="Olá, aqui você pode tirar suas dúvidas:"
         />
         <FormOrganism />
-      </div> */}
+      </div>
     </>
   );
 }
