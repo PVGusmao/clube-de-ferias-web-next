ed9940e6d1a3e9ec921c6fd8cdd1f63030c55b76
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-02-16 11:22:19 -0300

    ajustado renderização de carrocel dos beneficios

--- src/components/atomos/SlideShowBenefitsAboutUsAtom.tsx
+++ src/components/atomos/SlideShowBenefitsAboutUsAtom.tsx
@@ -8,6 +8,9 @@ import { BenefitsAboutUsMolecule } from "../moleculas/BenefitsAboutUsMolecule";
 import { SquaresInterface } from "../../constants/AboutUs";
 import { useContext } from "react";
 import { IMyContext, MyContext } from "../../context/MyContext";
+import { BsMap } from "react-icons/bs";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { IoMedalOutline } from "react-icons/io5";
 
 type Props = {
   className?: string;
@@ -16,10 +19,31 @@ type Props = {
 export const SlideShowBenefitsAboutUsAtom = ({ className }: Props) => {
   const { aboutUsTexts } = useContext(MyContext) as IMyContext;
 
+  const benefits = [
+    {
+      id: 1,
+      icon: BsMap,
+      title: aboutUsTexts.squares[0].title,
+      bodyText: aboutUsTexts.squares[0].subtitle
+    },
+    {
+      id: 2,
+      icon: AiOutlineSafetyCertificate,
+      title: aboutUsTexts.squares[1].title,
+      bodyText: aboutUsTexts.squares[1].subtitle
+    },
+    {
+      id: 3,
+      icon: IoMedalOutline,
+      title: aboutUsTexts.squares[2].title,
+      bodyText: aboutUsTexts.squares[2].subtitle
+    }
+  ]
+
   return (
     <div className={`${className} mt-10`}>
       <Slide arrows={false} transitionDuration={250}>
-        {aboutUsTexts.squares.map((element: SquaresInterface, index: number) => (
+        {benefits.map((element, index: number) => (
           <div className="h-30 flex items-center justify-center" key={index}>
             <BenefitsAboutUsMolecule
               element={element}
