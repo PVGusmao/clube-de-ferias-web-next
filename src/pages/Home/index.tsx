4816a91b207631921021bf8bd6ad85858cff1e4f
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-02-16 18:07:31 -0300

    refatorando plansOrganismo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ import { BannerOrganism } from "../../components/organismos/BannerOrganism";
 import { ClientsOrganism } from "../../components/organismos/ClientsOrganism";
 import { WelcomeOrganism } from "../../components/organismos/WelcomeOrganism";
 import { SimulationOrganism } from "../../components/organismos/SimulationOrganismo";
-import { Plans2Organism } from "../../components/organismos/Plans2Organism";
+import { PlansOrganism } from "../../components/organismos/PlansOrganism";
 
 export default function Home() {
   const { allSiteTexts } = useContext(MyContext) as IMyContext;
@@ -15,17 +15,21 @@ export default function Home() {
     <>
       <WelcomeOrganism data={allSiteTexts} />
 
-      <Plans2Organism data={allSiteTexts} />
+      <PlansOrganism data={allSiteTexts} />
 
       <SimulationOrganism data={allSiteTexts} />
 
-      {allSiteTexts?.data.paulo.banner[0].enable && <BannerOrganism data={allSiteTexts}/>}
+      {allSiteTexts?.data.paulo.banner[0].enable && (
+        <BannerOrganism data={allSiteTexts} />
+      )}
 
       <BlogOrganism />
 
       <ClientsOrganism />
 
-      {allSiteTexts?.data.paulo.banner[1].enable && <BannerOrganism data={allSiteTexts}/>}
+      {allSiteTexts?.data.paulo.banner[1].enable && (
+        <BannerOrganism data={allSiteTexts} />
+      )}
     </>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:6969/api",
+  baseURL: "http://10.0.1.137/api",
 });
 
 export default api;
