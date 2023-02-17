b4e5bf2a8ab99b845b90592c8b04825e7007ecd6
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-02-17 16:24:53 -0300

    implementado foto dos beneficios separadas no sobre nos

--- src/components/organismos/BenefitsAboutUsOrganism.tsx
+++ src/components/organismos/BenefitsAboutUsOrganism.tsx
@@ -4,13 +4,17 @@ import { IMyContext, MyContext } from "../../context/MyContext";
 import { ImageAtom } from "../atomos/ImageAtome";
 import { SlideShowBenefitsAboutUsAtom } from "../atomos/SlideShowBenefitsAboutUsAtom";
 
-import BenefitsAboutUs from "../../assets/Missao-Visao-Valores.png";
+import Missao from "../../assets/Missao.png";
+import Visao from "../../assets/Visao.png";
+import Valores from "../../assets/Valores.png";
 
 export function BenefitsAboutUsOrganism() {
   return (
     <>
-      <div className="hidden sm:flex justify-center mb-[200px]">
-        <ImageAtom className="mt-10" source={BenefitsAboutUs} alt="plans" />
+      <div className="hidden sm:flex mb-[200px] items-start justify-evenly">
+        <ImageAtom className="mt-10 w-[350px]" source={Missao} alt="plans" />
+        <ImageAtom className="mt-10 w-[350px]" source={Visao} alt="plans" />
+        <ImageAtom className="mt-10 w-[350px]" source={Valores} alt="plans" />
       </div>
       <SlideShowBenefitsAboutUsAtom className="sm:hidden" />
     </>
