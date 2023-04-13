e782e90d54463908e7f155e3b4dd89fe5c1d4891
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-04-13 18:17:07 -0300

    ajustada função para recuperar dados

--- pages/api/paulo/index.tsx
+++ pages/api/paulo/index.tsx
@@ -4,20 +4,19 @@ import { prisma } from "../../../config/db";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "GET":
-      const result = await prisma.pages.findMany();
-      const data = JSON.parse(result[0]?.content);
-      return res.status(200).json(result);
+      await getDataPages(req, res);
+      return
     default:
-      return res.status(200).json({message: 'Cara, melhora tua requisição ai, ta uma merda!'});
+      return res.status(200).json({message: 'Requisição não suportada.'});
   }
 }
 
-// const getDataPages = async (req: NextApiRequest, res: NextApiResponse) => {
-//   try {
-//     const result = await prisma.pages.findMany();
-//     console.log(result);
-//     return res.status(200).json({ result });
-//   } catch (err) {
-//     return res.status(200).json(err);
-//   }
-// }
+const getDataPages = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const result = await prisma.pages.findMany();
+    const data = JSON.parse(result[0]?.content);
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(200).json(err);
+  }
+}
